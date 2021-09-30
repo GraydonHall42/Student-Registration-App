6251f765594a18898e58c75e2707a424d6ea3782
GraydonHall42 <gwhall@ualberta.ca>
2021-09-30 15:46:25 -0600

    made test comment from main

--- src/View/StudentView.java
+++ src/View/StudentView.java
@@ -149,6 +149,7 @@ public class StudentView extends JFrame{
     public String getStudetnName(){
         return tfStudentName.getText();
         //this is a test comment
+        //this is a test comment from main
     }
 
     public String getStudentID(){
